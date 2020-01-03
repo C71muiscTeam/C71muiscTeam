ee242e3df9d057503441714a358cefea02d6d0fb
24630 <24630@LAPTOP-82EP1RMD>
2020-01-03 20:14:05 +0800

    项目创建

---
+++ C71-S3-Tstt-muisc/src/test/java/com/yc/C71S3pswthymeleaf/C71S3PswThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.C71S3pswthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C71S3PswThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
