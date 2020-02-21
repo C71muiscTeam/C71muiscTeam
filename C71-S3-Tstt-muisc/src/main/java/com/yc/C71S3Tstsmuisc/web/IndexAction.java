08e0f0c414c5d9112ce9c3c40d85b20d9b8a7fe4
Admin <Admin@DESKTOP-LC1F8UG>
2020-02-21 15:29:31 +0800

    无用页面，用来测试我的github是否能用

--- C71-S3-Tstt-muisc/src/main/java/com/yc/C71S3Tstsmuisc/web/IndexAction.java
+++ C71-S3-Tstt-muisc/src/main/java/com/yc/C71S3Tstsmuisc/web/IndexAction.java
@@ -1,6 +1,7 @@
 package com.yc.C71S3Tstsmuisc.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
